66cd6c3c7036fcc533fee23ccfba43736b823f26
Artem <art735@yandex.ru>
2024-09-14 16:06:52 +0300

    Refactoring

--- FitnessCalcMain.py
+++ FitnessCalcMain.py
@@ -1,15 +1,23 @@
-import datetime
 import os
 
 import gspread
 from oauth2client import service_account
 
-from oauth2client.service_account import ServiceAccountCredentials
-
 from FitnessCalcService import FitnessCalcService, ExerciseType
 
 # Указываем необходимые API
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+
+
+# Файл credentials.json – это файл, содержащий учётные данные, которые нужны для аутентификации и доступа к Google API.
+# В контексте работы с Google Sheets API или другими сервисами Google, он используется для предоставления вашему
+# приложению прав на доступ к ресурсам Google от имени пользователя.
+# Данный файл является конфиденциальным и его нельзя публиковать (в том числе и в GitHub-репозиторий).
+# Файл credentials.json хранится на локальном компьютере + вручную подкладывается на сервер (напр., Google Colab).
+# В тексте программы путь к файлу credentials.json вычитывается из переменной окружения (должна быть настроена как
+# на локальном компьютере, так и на сервере Google Colab) и таким образом происходит безопасная и абстрактная работа
+# с этим файлом (без засвечивания локальных путей в коде).
+
 
 # Лучший способ безопасно хранить и использовать конфиденциальные данные — через переменные окружения.
 # Загрузите файл credentials.json на сервер (или в защищённое хранилище).
@@ -51,8 +59,10 @@ class FitnessCalcMain:
         # dips - отжимания на брусьях
         dips_column_data = data[1]
 
-        pullups_current_state, pullups_average = self.fitnessCalcService.calculate_exercises(pullups_column_data, ExerciseType.PULL_UPS)
-        dips_current_state, dips_average = self.fitnessCalcService.calculate_exercises(dips_column_data, ExerciseType.DIPS)
+        pullups_current_state, pullups_average = self.fitnessCalcService.calculate_exercises(pullups_column_data,
+                                                                                             ExerciseType.PULL_UPS)
+        dips_current_state, dips_average = self.fitnessCalcService.calculate_exercises(dips_column_data,
+                                                                                       ExerciseType.DIPS)
 
         print(f'Current pull-ups: {pullups_current_state}')
         print(f'Current dips: {dips_current_state}')
